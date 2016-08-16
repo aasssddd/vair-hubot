04a3521975ff84bf6da42ea1650fc584211c6c61
William <william.chen@vair.com.tw>
2016-08-16 14:16:35 +0800

    hmmm....

--- config/uat/avantik.js
+++ config/uat/avantik.js
@@ -1,17 +1,17 @@
-// avantik.coffee
+// avantik.js
 module.exports = {
 	AVANTIK_AGENCY_CODE: "softmobile",
-	AVANTIK_ENDPOINT: "http://vairtest.tikaero.com/tikAeroWebAPI/BookingService.asmx?WSDL",
-	AVANTIK_FLIGHT_ENDPOINT: "http://vairtest.tikaero.com/FlightsWebservice/FlightsService.asmx?WSDL",
+	AVANTIK_ENDPOINT: "http://vair.avantik.io/TikAeroWebAPI_PM/BookingService.asmx?WSDL",
+	AVANTIK_FLIGHT_ENDPOINT: "http://vair.avantik.io/FlightsWebservice/FlightsService.asmx?WSDL",
 	AVANTIK_USER_ACCOUNT: "softmobile",
 	AVANTIK_USER_PASSWORD: "softmobile1234",
 	AVANTIK_QUERY_FLIGHT: "005;006;007;008;009;010",
-	SITA_CSV_FILE_PATH: "./temp_uat/",
-	AVANTIK_MESSAGE_ROOM: "martina_test_thaiapp",
-	SLACK_FILE_UPLOAD_TOKEN: "xoxp-4861959806-11435105106-15208321431-584424075c",
-	SITA_SEND_TIME_SHIFT: 0,
-	SITA_SEND_TIME_SHIFE_UNIT: "hour",
-	PASSENGER_MANIFEST_QUERY_FREQUENCY: "0 0 1 * * *",
+	SITA_CSV_FILE_PATH: "./temp/",
+	AVANTIK_MESSAGE_ROOM: "#general",
+	SLACK_FILE_UPLOAD_TOKEN: "slack file upload token",
+	SITA_SEND_TIME_SHIFT: 30,
+	SITA_SEND_TIME_SHIFE_UNIT: "minute",
+	PASSENGER_MANIFEST_QUERY_FREQUENCY: "0 * * * * *",
 	SITA_FILE_CHECK_TIMEOUT_SECOND: 3600,
 	MAX_RECORD_COUNT_PER_FILE: 80
 }
